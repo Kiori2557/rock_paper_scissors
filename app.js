99d3f7bdae618e0be9c419db30a3fe5d026e675a
Aung Khant Paing <akpaing99@gmail.com>
2023-06-19 19:15:55 +0630

    Add startTheGame Function
    startTheGame function to let the player play a total of 5 rounds

--- app.js
+++ app.js
@@ -4,20 +4,21 @@ function computerChoice() {
   return moves[Math.floor(Math.random() * 3)];
 }
 
-let computerMoves = computerChoice();
-let playerChoice = prompt("what is ur move? ").toLowerCase();
-
+let score = 0;
+let roundScore = 0;
+let playerChoice = "";
 function startTheRound(playerChoice, computerMoves) {
   console.log(`Computer chose ${computerMoves}`);
   console.log(`You chose ${playerChoice}`);
+  roundScore = 0;
+  let gameStatus = "";
+  //Comparison and result of the round
   if (
     playerChoice == "rock" ||
     playerChoice == "paper" ||
-    playerChoice == "scissors"
+    playerChoice == "scissors" ||
+    playerChoice == null
   ) {
-    let gameStatus = "";
-    let score = 0;
-    //Comparison and result of the round
     if (playerChoice == "rock") {
       switch (computerMoves) {
         case "rock":
@@ -25,45 +26,55 @@ function startTheRound(playerChoice, computerMoves) {
           break;
         case "paper":
           gameStatus = "Opps, u lose";
-          score -= 1;
+          roundScore -= 1;
           break;
         case "scissors":
           gameStatus = "Congrats!!, U won!";
-          score += 1;
+          roundScore += 1;
           break;
       }
     } else if (playerChoice == "paper") {
       switch (computerMoves) {
         case "rock":
           gameStatus = "Congrats!!, U won!";
-          score += 1;
+          roundScore += 1;
           break;
         case "paper":
           gameStatus = "Draw";
           break;
         case "scissors":
           gameStatus = "Opps, u lose";
-          score -= 1;
+          roundScore -= 1;
           break;
       }
     } else if (playerChoice == "scissors") {
       switch (computerMoves) {
         case "rock":
           gameStatus = "Opps, u lose";
-          score -= 1;
+          roundScore -= 1;
           break;
         case "paper":
           gameStatus = "Congrats!!, U won!";
           score += 1;
           break;
         case "scissors":
-          gameStatus = "Draw";
+          roundScore = "Draw";
           break;
       }
     }
-    console.log(score);
+    roundScore = roundScore;
   } else {
     console.log("Choose only from [rock,paper,scissors]");
   }
 }
-startTheRound(playerChoice, computerMoves);
+
+function startTheGame() {
+  for (i = 0; i < 5; i++) {
+    playerChoice = prompt("what is ur move? ").toLowerCase();
+    let computerMoves = computerChoice();
+    startTheRound(playerChoice, computerMoves);
+    score += roundScore;
+    console.log(score);
+  }
+}
+startTheGame();
