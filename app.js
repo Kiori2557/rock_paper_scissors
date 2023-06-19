8ff8db8ca2bafcb3805847a7dc92aa9b9d495e9a
Aung Khant Paing <akpaing99@gmail.com>
2023-06-19 18:35:36 +0630

    Add startTheRound Function
    startTheRound to play a round with computer and count the score

--- app.js
+++ app.js
@@ -3,16 +3,65 @@ function computerChoice() {
   let moves = ["rock", "paper", "scissors"];
   return moves[Math.floor(Math.random() * 3)];
 }
+
 let computerMoves = computerChoice();
 let playerChoice = prompt("what is ur move? ").toLowerCase();
+
 function startTheRound(playerChoice, computerMoves) {
-  console.log(playerChoice);
+  console.log(`Computer chose ${computerMoves}`);
+  console.log(`You chose ${playerChoice}`);
   if (
     playerChoice == "rock" ||
     playerChoice == "paper" ||
     playerChoice == "scissors"
   ) {
-    console.log(computerMoves);
+    let gameStatus = "";
+    let score = 0;
+    //Comparison and result of the round
+    if (playerChoice == "rock") {
+      switch (computerMoves) {
+        case "rock":
+          gameStatus = "Draw";
+          break;
+        case "paper":
+          gameStatus = "Opps, u lose";
+          score -= 1;
+          break;
+        case "scissors":
+          gameStatus = "Congrats!!, U won!";
+          score += 1;
+          break;
+      }
+    } else if (playerChoice == "paper") {
+      switch (computerMoves) {
+        case "rock":
+          gameStatus = "Congrats!!, U won!";
+          score += 1;
+          break;
+        case "paper":
+          gameStatus = "Draw";
+          break;
+        case "scissors":
+          gameStatus = "Opps, u lose";
+          score -= 1;
+          break;
+      }
+    } else if (playerChoice == "scissors") {
+      switch (computerMoves) {
+        case "rock":
+          gameStatus = "Opps, u lose";
+          score -= 1;
+          break;
+        case "paper":
+          gameStatus = "Congrats!!, U won!";
+          score += 1;
+          break;
+        case "scissors":
+          gameStatus = "Draw";
+          break;
+      }
+    }
+    console.log(score);
   } else {
     console.log("Choose only from [rock,paper,scissors]");
   }
